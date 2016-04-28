ba71286f3537aa905f17dff3cbb4695d8a46a774
Chris Keller <christopherlawrencekeller@gmail.com>
2016-04-28 16:53:42 -0700

    initial commit

---
+++ kpcc_backroom_handshakes/urls.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+from django.contrib import admin
+import os
+import logging
+
+logger = logging.getLogger("kpcc_backroom_handshakes")
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
+    url(r"^admin/", include(admin.site.urls)),
+
+    # batch edit in admin
+    url(r"^admin/", include("massadmin.urls")),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
+
+if settings.DEBUG and settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
