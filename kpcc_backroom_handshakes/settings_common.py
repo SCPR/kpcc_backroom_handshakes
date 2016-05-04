d4ac24ac39cc0802cbd93d6e9b4bf60316871651
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-04 12:49:02 -0700

    follows django 1.8 convention for carrying settings for template engines inside a list

--- kpcc_backroom_handshakes/settings_common.py
+++ kpcc_backroom_handshakes/settings_common.py
@@ -37,24 +37,31 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
-TEMPLATE_DIRS = (os.path.join(SITE_ROOT, "templates"),)
-
-TEMPLATE_LOADERS = (
-    "django.template.loaders.filesystem.Loader",
-    "django.template.loaders.app_directories.Loader",
-    #"django.template.loaders.eggs.Loader",
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.request",
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages"
-)
+TEMPLATES = [
+    {
+        'TEMPLATE_DEBUG': 'DEBUG',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+            # (os.path.join(SITE_ROOT, "templates"),)
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                "django.core.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.core.context_processors.debug",
+                "django.core.context_processors.i18n",
+                "django.core.context_processors.media",
+                "django.core.context_processors.static",
+                "django.core.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 MIDDLEWARE_CLASSES = (
     #"django.contrib.auth.middleware.SessionAuthenticationMiddleware",
