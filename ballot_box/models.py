81377820c49d28dfec8ef681e7eff71b37ce05ed
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-04 13:47:40 -0700

    updates the ballot box model to give each source an election date

--- ballot_box/models.py
+++ ballot_box/models.py
@@ -10,6 +10,7 @@ class ResultSource(models.Model):
     source_short = models.CharField("Shortname of data source", max_length=5)
     source_slug = models.SlugField("Slugged data soure", db_index=True, unique=True, max_length=255, null=True, blank=True)
     source_url = models.URLField("URL to data source", max_length=1024, null=True, blank=True)
+    source_election_date = models.DateTimeField("Date of election for this source", null=True, blank=True)
     source_active = models.BooleanField("Active data source?", default=False)
     source_type = models.CharField("Ext of file or type of source", max_length=255, null=False, blank=False)
     source_created = models.DateTimeField("Date Created", auto_now_add=True)
