6f16a5ef290e329d2d0d7c7ecc24281f18e1505a
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-05 22:26:06 -0700

    updates my models to give election the parent role

--- ballot_box/models.py
+++ ballot_box/models.py
@@ -2,30 +2,6 @@ from __future__ import unicode_literals
 from django.db import models
 from kpcc_backroom_handshakes.custom_fields import ListField
 
-class ResultSource(models.Model):
-    """
-    describes a source of data for election results
-    """
-    source_name = models.CharField("Name of data source", db_index=True, unique=True, max_length=255)
-    source_short = models.CharField("Shortname of data source", max_length=5)
-    source_slug = models.SlugField("Slugged data soure", db_index=True, unique=True, max_length=255, null=True, blank=True)
-    source_url = models.URLField("URL to data source", max_length=1024, null=True, blank=True)
-    source_election_date = models.DateField("Date of election for this source", null=True, blank=True)
-    source_active = models.BooleanField("Active data source?", default=False)
-    source_type = models.CharField("Ext of file or type of source", max_length=255, null=False, blank=False)
-    source_files = ListField("Results Files We Want", null=True, blank=True)
-    source_created = models.DateTimeField("Date Created", auto_now_add=True)
-    source_modified = models.DateTimeField("Date Modified", auto_now=True)
-
-
-    def __unicode__(self):
-        return self.source_name
-
-
-    def save(self, *args, **kwargs):
-        super(ResultSource, self).save(*args, **kwargs)
-
-
 class Election(models.Model):
     """
     describes an election that will have results we want to capture
@@ -62,7 +38,7 @@ class Election(models.Model):
 
 
     def __unicode__(self):
-        return self.type
+        return self.unique_id
 
 
     def save(self, *args, **kwargs):
@@ -70,3 +46,27 @@ class Election(models.Model):
             self.election_date_str = self.election_date.strftime("%Y-%m-%d")
             self.unique_id = "%s-%s" % (self.type.lower(), self.election_date_str)
         super(Election, self).save(*args, **kwargs)
+
+
+class ResultSource(models.Model):
+    """
+    describes a source of data for election results
+    """
+    election = models.ForeignKey(Election)
+    source_name = models.CharField("Name of data source", db_index=True, unique=True, max_length=255)
+    source_short = models.CharField("Shortname of data source", max_length=5)
+    source_slug = models.SlugField("Slugged data soure", db_index=True, unique=True, max_length=255, null=True, blank=True)
+    source_url = models.URLField("URL to data source", max_length=1024, null=True, blank=True)
+    source_active = models.BooleanField("Active data source?", default=False)
+    source_type = models.CharField("Ext of file or type of source", max_length=255, null=False, blank=False)
+    source_files = ListField("Results Files We Want", null=True, blank=True)
+    source_created = models.DateTimeField("Date Created", auto_now_add=True)
+    source_modified = models.DateTimeField("Date Modified", auto_now=True)
+
+
+    def __unicode__(self):
+        return self.source_name
+
+
+    def save(self, *args, **kwargs):
+        super(ResultSource, self).save(*args, **kwargs)
