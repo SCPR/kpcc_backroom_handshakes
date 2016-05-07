4227a6c6a0d1a9fdf88ef4c76bca55b712d8d970
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-06 17:11:11 -0700

    updates to the base file_utils

--- ballot_box/utils_files.py
+++ ballot_box/utils_files.py
@@ -6,6 +6,8 @@ import time
 import datetime
 import shutil
 import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 import zipfile
 
 logger = logging.getLogger("kpcc_backroom_handshakes")
@@ -24,32 +26,51 @@ class Retriever(object):
         """
         can i take the response from url can and write it to a timestamped version of the a file that should work no matter the file. it's  based on the file_ext specified in a config dict
         """
+        session = requests.Session()
+        retries = Retry(
+            total=5,
+            backoff_factor=1,
+            status_forcelist=[500, 502, 503, 504]
+        )
+        session.mount("http://", HTTPAdapter(max_retries=retries))
+        response = session.get(
+            item.source_url,
+            headers=settings.REQUEST_HEADERS,
+            timeout=10,
+            allow_redirects=False
+        )
         try:
-            response = requests.get(item.source_url, headers=settings.REQUEST_HEADERS, stream=True)
-            with open(item.file_name, "wb") as output:
-                output.write(response.content)
-            file_exists = os.path.isfile(item.file_name)
-            file_has_size = os.path.getsize(item.file_name)
-            if file_exists == True and file_has_size > 0:
+            response.raise_for_status()
+            logger.debug("%s: success" % (item.source_url))
+        except requests.exceptions.ConnectionError as exception:
+            # incorrect domain
+            logger.error("%s: %s" % (exception, item.source_url))
+            raise
+        except requests.exceptions.Timeout as exception:
+            # maybe set up for a retry, or continue in a retry loop
+            logger.error("%s: %s" % (exception, item.source_url))
+            raise
+        except requests.exceptions.TooManyRedirects as exception:
+            # tell the user their url was bad and try a different one
+            logger.error("%s: %s" % (exception, item.source_url))
+            raise
+        except requests.exceptions.RequestException as exception:
+            # catastrophic error and bail
+            logger.error("%s: %s" % (exception, item.source_url))
+            sys.exit(1)
+        with open(item.file_name, "wb") as output:
+            output.write(response.content)
+        file_exists = os.path.isfile(item.file_name)
+        file_has_size = os.path.getsize(item.file_name)
+        if file_exists == True:
+            if file_has_size > 0:
                 logger.debug("Success!")
-        except Exception, exception:
-            logger.error(exception)
-            raise
-
-    # REFACTOR FUNCTION TO ACCOUNT FOR MULTIPLE FILE TYPES
-    def _found_files_in_zipfile(self, item):
-        """
-        compare files in a zipfile with a list of expected files
-        """
-        try:
-            with zipfile.ZipFile(item.file_name) as zip:
-                files = zipfile.ZipFile.namelist(zip)
-                for item in self.list_of_expected_files:
-                    if item.strip() in set(files) == True:
-                        logger.debug("Success!")
-        except Exception, exception:
-            logger.error(exception)
-            raise
+            else:
+                logger.error("Your file has zero data")
+                raise Exception
+        else:
+            logger.error("Your file doesn't exist")
+            raise Exception
 
 
     def _copy_timestamped_file_as_latest(self, item, data_directory):
@@ -110,6 +131,31 @@ class Retriever(object):
             logger.debug("Success!")
 
 
+    def _found_required_files(self, item, data_directory):
+        """
+        compare files in a zipfile with a list of expected files
+        """
+        working = "%s%s_latest" % (data_directory, item.source_short)
+        latest = os.path.join(working, os.path.basename(item.file_latest))
+        if item.source_type == ".zip":
+            try:
+                with zipfile.ZipFile(latest) as zip:
+                    files = zipfile.ZipFile.namelist(zip)
+                    for file in self.list_of_expected_files:
+                        self.assertEquals(file.strip() in set(files), True)
+                        logger.debug("Success: %s exists" % (file.strip()))
+            except Exception, exception:
+                logger.error(exception)
+        else:
+            try:
+                for file in self.list_of_expected_files:
+                    self.assertEquals(file.strip(), os.path.basename(item.file_latest))
+                    logger.debug("Success: %s exists" % (file.strip()))
+            except Exception, exception:
+                logger.error(exception)
+                raise
+
+
     def _unzip_latest_file(self, item, data_directory):
         """
         if the item is a zipfile can I extract the files?
@@ -119,8 +165,12 @@ class Retriever(object):
             file_latest = os.path.join(working, os.path.basename(item.file_latest))
             with zipfile.ZipFile(file_latest) as zip:
                 if zipfile.ZipFile.testzip(zip) == None:
-                    zip.extract("X14GG510v7.xml", working)
-                    zip.extract("X14GG530v7.xml", working)
+                    for file in self.list_of_expected_files:
+                        file = file.strip()
+                        zip.extract(file, working)
+                        file_exists = os.path.isfile(os.path.join(working, file))
+                        if file_exists == True:
+                            logger.debug("Success!")
                 os.remove(file_latest)
                 file_exists = os.path.isfile(file_latest)
                 if file_exists == False:
