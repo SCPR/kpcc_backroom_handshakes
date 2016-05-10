78ba3ca283e1e0d4bccedd309a47c899d1b0e579
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-10 14:50:17 -0700

    applies pep8 style to ballot box models

--- ballot_box/models.py
+++ ballot_box/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 from kpcc_backroom_handshakes.custom_fields import ListField
 
+
 class Election(models.Model):
     """
     describes an election that will have results we want to capture
@@ -19,32 +20,36 @@ class Election(models.Model):
     type = models.CharField(
         "Type of Election",
         max_length=255,
-        choices = ELECTION_TYPE_CHOICES,
-        default = PRIMARY,
+        choices=ELECTION_TYPE_CHOICES,
+        default=PRIMARY,
     )
 
-    electionid = models.CharField("Election ID", max_length=255, null=True, blank=True)
+    electionid = models.CharField(
+        "Election ID", max_length=255, null=True, blank=True)
     test_results = models.BooleanField("Are These Test Results", default=False)
     live_results = models.BooleanField("Are These Live Results", default=False)
-    election_date = models.DateField("Date of the Election", null=True, blank=True)
-    poll_close_at = models.DateTimeField("Time the Polls Close", null=True, blank=True)
-    national = models.BooleanField("Is this a National Election?", default=False)
+    election_date = models.DateField(
+        "Date of the Election", null=True, blank=True)
+    poll_close_at = models.DateTimeField(
+        "Time the Polls Close", null=True, blank=True)
+    national = models.BooleanField(
+        "Is this a National Election?", default=False)
     # parsed_json
+
     # next_request
     # datafile
     # results_level
     created = models.DateTimeField("Date Created", auto_now_add=True)
     modified = models.DateTimeField("Date Modified", auto_now=True)
 
-
     def __unicode__(self):
         return self.electionid
-
 
     def save(self, *args, **kwargs):
         if not self.electionid:
             self.election_date_str = self.election_date.strftime("%Y-%m-%d")
-            self.electionid = "%s-%s" % (self.type.lower(), self.election_date_str)
+            self.electionid = "%s-%s" % (self.type.lower(),
+                                         self.election_date_str)
         super(Election, self).save(*args, **kwargs)
 
 
@@ -53,20 +58,22 @@ class ResultSource(models.Model):
     describes a source of data for election results
     """
     election = models.ForeignKey(Election)
-    source_name = models.CharField("Name of data source", db_index=True, unique=True, max_length=255)
+    source_name = models.CharField(
+        "Name of data source", db_index=True, unique=True, max_length=255)
     source_short = models.CharField("Shortname of data source", max_length=5)
-    source_slug = models.SlugField("Slugged data soure", db_index=True, unique=True, max_length=255, null=True, blank=True)
-    source_url = models.URLField("URL to data source", max_length=1024, null=True, blank=True)
+    source_slug = models.SlugField(
+        "Slugged data soure", db_index=True, unique=True, max_length=255, null=True, blank=True)
+    source_url = models.URLField(
+        "URL to data source", max_length=1024, null=True, blank=True)
     source_active = models.BooleanField("Active data source?", default=False)
-    source_type = models.CharField("Ext of file or type of source", max_length=255, null=False, blank=False)
+    source_type = models.CharField(
+        "Ext of file or type of source", max_length=255, null=False, blank=False)
     source_files = ListField("Results Files We Want", null=True, blank=True)
     source_created = models.DateTimeField("Date Created", auto_now_add=True)
     source_modified = models.DateTimeField("Date Modified", auto_now=True)
 
-
     def __unicode__(self):
         return self.source_name
-
 
     def save(self, *args, **kwargs):
         super(ResultSource, self).save(*args, **kwargs)
