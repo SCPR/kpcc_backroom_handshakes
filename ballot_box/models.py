c432582e7e3d85b007f6a9561d740ee0b915ce9a
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-15 01:46:00 -0700

    updates to the ballot box models to work with multiple sources

--- ballot_box/models.py
+++ ballot_box/models.py
@@ -63,12 +63,12 @@ class ResultSource(models.Model):
     source_short = models.CharField("Shortname Of Data Source", max_length=5)
     source_slug = models.SlugField(
         "Slugged Data Soure", db_index=True, unique=True, max_length=255, null=True, blank=True)
-    source_url = models.URLField(
-        "Url To Data Source", max_length=1024, null=True, blank=True)
+    source_url = models.URLField("Url To Data Source", max_length=1024, null=True, blank=True)
     source_active = models.BooleanField("Active Data Source?", default=False)
     source_type = models.CharField(
         "Ext Of File Or Type Of Source", max_length=255, null=False, blank=False)
     source_files = ListField("Results Files We Want", null=True, blank=True)
+    source_latest = models.DateTimeField("Latest Results From", null=True, blank=True)
     source_created = models.DateTimeField("Date Created", auto_now_add=True)
     source_modified = models.DateTimeField("Date Modified", auto_now=True)
 
@@ -119,6 +119,8 @@ class Contest(models.Model):
     is_ballot_measure = models.BooleanField("Is A Measure?", default=False)
     is_judicial = models.BooleanField("Is Judicial Contest?", default=False)
     is_runoff = models.BooleanField("Is A Runoff Contest?", default=False)
+    reporttype = models.CharField(
+        "Status of Results", max_length=255, null=True, blank=True)
     precinctstotal = models.IntegerField(
         "Total Number Of Precincts", null=True, blank=True)
     precinctsreporting = models.IntegerField(
