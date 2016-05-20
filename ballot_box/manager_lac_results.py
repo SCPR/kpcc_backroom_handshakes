535cc5b9977d738fa7946dfcdf1b19b215ee507f
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-20 15:40:48 -0700

    makes shortcode in source retrival

--- ballot_box/manager_lac_results.py
+++ ballot_box/manager_lac_results.py
@@ -27,7 +27,7 @@ class BuildLacResults(object):
     data_directory = "%s/ballot_box/data_dump/" % (settings.BASE_DIR)
 
     sources = ResultSource.objects.filter(
-        source_short="lac98", source_active=True)
+        source_short="lac", source_active=True)
 
     def _init(self, *args, **kwargs):
         """
@@ -103,7 +103,7 @@ class BuildLacResults(object):
             file_timestamp = localtime(file_timestamp)
             update_this = saver._eval_timestamps(
                 file_timestamp, src.source_latest)
-            update_this = True # delete after update_this works and you have a live data source 
+            update_this = True # delete after update_this works and you have a live data source
             if update_this == False:
                 logger.debug(
                     "We have newer data in the database so let's delete these files.")
@@ -507,7 +507,7 @@ class LacProcessMethods(object):
         candidates = contest_package['candidates']
         measures = contest_package['measures']
         judges = contest_package['judges']
-        
+
         if contest['is_judicial_contest']:
             """ This is a judicial appointee """
             contestname = (contest['contest_title'] + ' ' + contest['contest_title_cont']).title()
@@ -902,7 +902,7 @@ class LacProcessMethods(object):
                 f.write('N/A\n')
 
     def prettyprint(self, election_info, contest_list, measure_list, candidate_list, judicial_list):
-        """Prints human readable, detailed layout of all candidates, contests, measures, and 
+        """Prints human readable, detailed layout of all candidates, contests, measures, and
         election stats. Activate by un-commenting call in evaluate_and_process_races().
         """
         report = ''
