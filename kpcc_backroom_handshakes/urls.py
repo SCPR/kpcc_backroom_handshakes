149c83e2cb9b9de8ff0144a83c100579995095fe
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-26 00:49:10 -0700

    implements tastypie api as url resources

--- kpcc_backroom_handshakes/urls.py
+++ kpcc_backroom_handshakes/urls.py
@@ -4,14 +4,26 @@ from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 from django.contrib import admin
+from tastypie.api import Api
+from kpcc_backroom_handshakes.api import CandidateResource, MeasureResource, JudicialResource, ContestResource
 import os
 import logging
 
 logger = logging.getLogger("kpcc_backroom_handshakes")
 
+# invoke the api
+v1_api = Api(api_name='v1')
+v1_api.register(CandidateResource())
+v1_api.register(MeasureResource())
+v1_api.register(JudicialResource())
+v1_api.register(ContestResource())
+
 admin.autodiscover()
 
 urlpatterns = [
+
+    # tastypie api
+    url(r'^api/?', include(v1_api.urls)),
     url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^admin/", include("massadmin.urls")),
