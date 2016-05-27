6659d43094016f4944bf7d64802d78cfb4a881c5
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-27 04:36:52 -0400

    updates and fixes to settings that I had broken

--- kpcc_backroom_handshakes/wsgi.py
+++ kpcc_backroom_handshakes/wsgi.py
@@ -9,12 +9,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ["CONFIG_PATH"] = "%s_PRODUCTION" % ("kpcc_backroom_handshakes".upper())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kpcc_backroom_handshakes.settings_production")
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
