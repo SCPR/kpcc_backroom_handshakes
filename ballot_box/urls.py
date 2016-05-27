30d1c0c5515a7a71a8e8cf3e67beceda9550c38e
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-27 18:54:32 -0400

    updates to location of static assets

--- ballot_box/urls.py
+++ ballot_box/urls.py
@@ -8,6 +8,7 @@ app_name = "ballot_box"
 
 urlpatterns = [
     url(r'results/$', ResultIndex.as_view(), name="result-index"),
+    url(r'results/index.html$', ResultIndex.as_view(), name="result-index"),
     # url(r'^events/add/$', EventCreate.as_view(), name='event-add'),
     # url(r'^events/(?P<pk>[0-9]+)/$', EventUpdate.as_view(), name='event-update'),
     # url(r'^events/(?P<pk>[0-9]+)/delete/$', EventDelete.as_view(), name='event-delete'),
