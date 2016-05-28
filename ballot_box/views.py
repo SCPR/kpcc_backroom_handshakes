3f04969cc74c6b995ce7928d71326b47366540e8
Chris Keller <christopherlawrencekeller@gmail.com>
2016-05-28 15:56:32 -0700

    updates to the views to create a bakeable view for results

--- ballot_box/views.py
+++ ballot_box/views.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest, Http404
 from django.views.decorators.clickjacking import xframe_options_exempt, xframe_options_sameorigin
 from django.views.generic import View, ListView, DetailView
+from bakery.views import BuildableListView, BuildableDetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.db.models import Q, Avg, Max, Min, Sum, Count
@@ -17,55 +18,40 @@ class ResultIndex(ListView):
 
     model = Contest
 
+    template_name = "ballot_box/index.html"
+
     def get_context_data(self, **kwargs):
         context = super(ResultIndex, self).get_context_data(**kwargs)
         queryset = Contest.objects.filter(election__electionid="primary-2016-06-07")
-
         context["featured_list"] = queryset.filter(is_homepage_priority=True)
-
         context["house_rep_list"] = queryset.filter(
             contestid__contains="sos-districtwide-us-house-of-representatives"
         )
-
         context["state_list"] = queryset.filter(
             Q(contestid__contains="sos-districtwide-state-senate") |
             Q(contestid__contains="sos-districtwide-state-assembly")
         ).order_by("contestid")
-
         return context
 
+class BakedResultsIndex(BuildableListView):
 
-# class EventForm(forms.ModelForm):
-#     class Meta:
-#         model = Event
-#         fields = [
-#             'created_at',
-#             'event_type',
-#             'event_notes',
-#         ]
+    model = Contest
 
-#         widgets = {
-#             'event_type': forms.RadioSelect,
-#         }
+    template_name = "ballot_box/index.html"
 
+    def get_object(self):
+        object = super(BakedResultsIndex, self).get_object()
+        return object
 
-# class EventCreate(CreateView):
-#     model = Event
-#     form_class = EventForm
-#     success_url = reverse_lazy('infant_tracker:event-index')
-
-
-# class EventUpdate(UpdateView):
-#     model = Event
-#     fields = [
-#         'created_at',
-#         'event_type',
-#         'event_notes',
-#     ]
-#     template_name_suffix = '_update_form'
-#     success_url = reverse_lazy('infant_tracker:event-index')
-
-
-# class EventDelete(DeleteView):
-#     model = Event
-#     success_url = reverse_lazy('infant_tracker:event-index')
+    def get_context_data(self, **kwargs):
+        context = super(BakedResultsIndex, self).get_context_data(**kwargs)
+        queryset = Contest.objects.filter(election__electionid="primary-2016-06-07")
+        context["featured_list"] = queryset.filter(is_homepage_priority=True)
+        context["house_rep_list"] = queryset.filter(
+            contestid__contains="sos-districtwide-us-house-of-representatives"
+        )
+        context["state_list"] = queryset.filter(
+            Q(contestid__contains="sos-districtwide-state-senate") |
+            Q(contestid__contains="sos-districtwide-state-assembly")
+        ).order_by("contestid")
+        return context
