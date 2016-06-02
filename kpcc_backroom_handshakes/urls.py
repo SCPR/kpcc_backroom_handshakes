65b2835feb67264c1303217d39ff369a5a2e3ee4
Chris Keller <christopherlawrencekeller@gmail.com>
2016-06-01 22:28:10 -0400

    fixes admin urls

--- kpcc_backroom_handshakes/urls.py
+++ kpcc_backroom_handshakes/urls.py
@@ -20,9 +20,9 @@ v1_api.register(ContestResource())
 admin.autodiscover()
 
 urlpatterns = [
-    url(r"^admin/doc/?", include("django.contrib.admindocs.urls")),
-    url(r"^admin/?", include(admin.site.urls)),
-    url(r"^admin/?", include("massadmin.urls")),
+    url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^admin/", include("massadmin.urls")),
     url(r"^api/?", include(v1_api.urls)),
     url(r"", include("ballot_box.urls", namespace="ballot-box")),
 ]
