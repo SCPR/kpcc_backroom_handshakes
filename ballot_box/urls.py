7aa8aa6a5d07b14c65f28b07c48ecd7c87d5c279
Chris Keller <christopherlawrencekeller@gmail.com>
2016-06-02 00:46:45 -0700

    creates a second view to display homepage races

--- ballot_box/urls.py
+++ ballot_box/urls.py
@@ -2,13 +2,16 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 from django.views.decorators.cache import cache_page
-from .views import ResultIndex, BakedResultsIndex
+from .views import FeaturedIndex, ResultIndex, BakedFeaturedIndex, BakedResultsIndex
 
 app_name = "ballot_box"
 
 urlpatterns = [
-    url(r'ballot-box/results/index.html$', cache_page(60*10)(BakedResultsIndex.as_view()), name="baked-result-index"),
-    url(r'ballot-box/results/$', ResultIndex.as_view(), name="result-index"),
-    url(r'ballot-box/$', RedirectView.as_view(url="results/", permanent=False), name='index'),
-    url(r'', RedirectView.as_view(url="ballot-box/results/", permanent=False), name='index'),
+    url(r'ballot-box/results/featured/index.html$', cache_page(60*10)(BakedFeaturedIndex.as_view()), name="baked-featured"),
+    url(r'ballot-box/results/featured/$', FeaturedIndex.as_view(), name="featured-index"),
+    url(r'ballot-box/results/all/index.html$', cache_page(60*10)(BakedResultsIndex.as_view()), name="baked-results"),
+    url(r'ballot-box/results/all/$', ResultIndex.as_view(), name="result-index"),
+    # url(r'ballot-box/results$', RedirectView.as_view(url="all/", permanent=False), name='index'),
+    url(r'ballot-box/$', RedirectView.as_view(url="results/all/", permanent=False), name='index'),
+    url(r'', RedirectView.as_view(url="ballot-box/results/all/", permanent=False), name='index'),
 ]
--- ballot_box/views.py
+++ ballot_box/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import get_object_or_404, render_to_response, render
 from django.template import RequestContext, Context, loader
 from django.template.loader import render_to_string
@@ -10,33 +11,65 @@ from django.core.urlresolvers import reverse, reverse_lazy
 from django.db.models import Q, Avg, Max, Min, Sum, Count
 from django import forms
 from .models import *
+import os
 import logging
 
 logger = logging.getLogger("kpcc_backroom_handshakes")
 
-class ResultIndex(ListView):
 
+class FeaturedIndex(ListView):
     model = Contest
+    template_name = "ballot_box/featured.html"
 
-    template_name = "ballot_box/index.html"
+    def get_context_data(self, **kwargs):
+        context = super(FeaturedIndex, self).get_context_data(**kwargs)
+        queryset = Contest.objects.filter(
+            election__electionid="primary-2016-06-07")
+        context["featured_list"] = queryset.filter(is_homepage_priority=True)
+        return context
+
+
+class BakedFeaturedIndex(BuildableListView):
+    model = Contest
+    template_name = "ballot_box/featured.html"
+    build_path = "featured.html"
+
+    def get_object(self):
+        object = super(BakedFeaturedIndex, self).get_object()
+        return object
+
+    def get_context_data(self, **kwargs):
+        context = super(BakedFeaturedIndex, self).get_context_data(**kwargs)
+        queryset = Contest.objects.filter(
+            election__electionid="primary-2016-06-07")
+        context["featured_list"] = queryset.filter(is_homepage_priority=True)
+        return context
+
+
+class ResultIndex(ListView):
+    model = Contest
+    template_name = "ballot_box/list.html"
 
     def get_context_data(self, **kwargs):
         context = super(ResultIndex, self).get_context_data(**kwargs)
-        queryset = Contest.objects.filter(election__electionid="primary-2016-06-07")
+        queryset = Contest.objects.filter(
+            election__electionid="primary-2016-06-07")
         context["featured_list"] = queryset.filter(is_homepage_priority=True)
-        context["local_list"] = queryset.filter(resultsource__source_short="lac").filter(is_display_priority=True)
-        context["house_rep_list"] = queryset.filter(contestid__contains="sos-districtwide-us-house-of-representatives")
+        context["local_list"] = queryset.filter(
+            resultsource__source_short="lac").filter(is_display_priority=True)
+        context["house_rep_list"] = queryset.filter(
+            contestid__contains="sos-districtwide-us-house-of-representatives")
         context["state_list"] = queryset.filter(
             Q(contestid__contains="sos-districtwide-state-senate") |
             Q(contestid__contains="sos-districtwide-state-assembly")
         ).order_by("contestid")
         return context
 
+
 class BakedResultsIndex(BuildableListView):
-
     model = Contest
-
-    template_name = "ballot_box/index.html"
+    template_name = "ballot_box/list.html"
+    build_path = "all.html"
 
     def get_object(self):
         object = super(BakedResultsIndex, self).get_object()
@@ -44,7 +77,8 @@ class BakedResultsIndex(BuildableListView):
 
     def get_context_data(self, **kwargs):
         context = super(BakedResultsIndex, self).get_context_data(**kwargs)
-        queryset = Contest.objects.filter(election__electionid="primary-2016-06-07")
+        queryset = Contest.objects.filter(
+            election__electionid="primary-2016-06-07")
         context["featured_list"] = queryset.filter(is_homepage_priority=True)
         context["house_rep_list"] = queryset.filter(
             contestid__contains="sos-districtwide-us-house-of-representatives"
