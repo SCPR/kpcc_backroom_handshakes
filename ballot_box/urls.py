7aba5616a141be1c3c44a6c15e3026c01e82c976
Chris Keller <christopherlawrencekeller@gmail.com>
2016-09-12 14:39:06 -0700

    removes latest static files and adds cacheing to current views

--- ballot_box/urls.py
+++ ballot_box/urls.py
@@ -6,47 +6,43 @@ from .views import FeaturedIndex, ResultIndex, BakedFeaturedIndex, BakedResultsI
 
 app_name = "ballot_box"
 
+cache_timer = 60 * 5
+
 urlpatterns = [
 
     url(
         r"(?P<electionid>[-\w]+)/results/featured/?$",
-        FeaturedIndex.as_view(),
+        cache_page(cache_timer)(FeaturedIndex.as_view()),
         name="featured-index"
     ),
 
     url(
         r"(?P<electionid>[-\w]+)/results/featured/index.html$",
-        cache_page(60*10)(BakedFeaturedIndex.as_view()),
+        cache_page(cache_timer)(BakedFeaturedIndex.as_view()),
         name="baked-featured"
     ),
 
     url(
         r"(?P<electionid>[-\w]+)/results/all/?$",
-        ResultIndex.as_view(),
+        cache_page(cache_timer)(ResultIndex.as_view()),
         name="result-index"
     ),
 
     url(
         r"(?P<electionid>[-\w]+)/results/all/index.html$",
-        cache_page(60*10)(BakedResultsIndex.as_view()),
+        cache_page(cache_timer)(BakedResultsIndex.as_view()),
         name="baked-results"
     ),
 
     url(
-        r"(?P<electionid>[-\w]+)/results/?$",
-        RedirectView.as_view(url="all/", permanent=False),
+        r"(?P<electionid>[-\w]+)/results$",
+        RedirectView.as_view(url="results/all/", permanent=False),
         name="result-index"
     ),
 
     url(
-        r"latest-results/featured/?$",
-        FeaturedIndex.as_view(),
-        name="featured-index"
-    ),
-
-    url(
-        r"latest-results/all/?$",
-        ResultIndex.as_view(),
+        r"(?P<electionid>[-\w]+)/results/$",
+        RedirectView.as_view(url="all/", permanent=False),
         name="result-index"
     ),
 
