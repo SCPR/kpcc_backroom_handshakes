a89154be4e19b8f22c330f533b36dc532a85615a
Chris Keller <christopherlawrencekeller@gmail.com>
2016-10-21 14:43:39 -0700

    updates the view to order races by name

--- ballot_box/views.py
+++ ballot_box/views.py
@@ -95,22 +95,22 @@ class ResultIndex(ListView):
                 Q(contestid__contains="sos-statewide-president") |
                 Q(contestid__contains="sos-statewide-us-senate") |
                 Q(contestid__contains="sos-districtwide-united-states-representative")
-        )
+        ).order_by("contestname")
         context["state_races"] = Contest.objects.filter(election__electionid=context["electionid"]).filter(
             is_display_priority=True).filter(is_ballot_measure=False).filter(
                 Q(contestid__contains="sos-districtwide-state-senate") |
                 Q(contestid__contains="sos-districtwide-state-assembly")
-        )
+        ).order_by("contestname")
         context["local_races"] = Contest.objects.filter(election__electionid=context["electionid"]).filter(
             is_display_priority=True).filter(is_ballot_measure=False).filter(
                 Q(resultsource__source_short="lac") |
                 Q(resultsource__source_short="oc")
-        )
-        context["state_measures"] = queryset.filter(is_ballot_measure=True).filter(resultsource__source_short="sos")
+        ).order_by("contestname")
+        context["state_measures"] = queryset.filter(is_ballot_measure=True).filter(resultsource__source_short="sos").order_by("contestname")
         context["local_measures"] = queryset.filter(is_ballot_measure=True).filter(
             Q(resultsource__source_short="lac") |
             Q(resultsource__source_short="oc")
-        )
+        ).order_by("contestname")
         return context
 
 
