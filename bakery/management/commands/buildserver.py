df53ddf09ea7e9e9aaac48d67aa86083e0d1a371
Chris Keller <christopherlawrencekeller@gmail.com>
2016-10-25 22:03:51 -0700

    updates buildserver.py to remove line break

--- bakery/management/commands/buildserver.py
+++ bakery/management/commands/buildserver.py
@@ -3,8 +3,7 @@ from django.core.management.commands import runserver
 
 
 class Command(runserver.Command):
-    help = "Starts a variation of Django's runserver designed to serve \
-the static files you've built."
+    help = "Starts a variation of Django's runserver designed to serve the static files you've built."
 
     @override_settings(ROOT_URLCONF='bakery.static_urls')
     def handle(self, *args, **kwds):
