dd78687cddea6d976d06b4cba81bfc1327fbb950
Chris Keller <christopherlawrencekeller@gmail.com>
2016-11-10 09:32:36 -0800

    updates close measures template

--- newscast/urls.py
+++ newscast/urls.py
@@ -7,15 +7,9 @@ from newscast.views import NewscastIndexView, NewscastMapView, NewscastDetailVie
 app_name = "newscast"
 
 urlpatterns = [
-
     url(r"(?P<electionid>[-\w]+)/playlist/map-presidential/$", NewscastMapView.as_view(), name="newscast_presidential_map"),
-
     url(r"(?P<electionid>[-\w]+)/playlist/close-measures/$", NewscastCloseMeasuresView.as_view(), name="newscast_close_measures"),
-
     url(r"(?P<electionid>[-\w]+)/playlist/close-races/$", NewscastCloseRacesView.as_view(), name="newscast_close"),
-
     url(r"(?P<electionid>[-\w]+)/playlist/(?P<slug>[-\w]+)/$", NewscastDetailView.as_view(), name="newscast_detail"),
-
     url(r"(?P<electionid>[-\w]+)/playlist/$", NewscastIndexView.as_view(), name="newscast_list"),
-
 ]
